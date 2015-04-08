2a4e87f0e9236b5ef3545b749cb29120b58d3054
JRice <jrice@eol.org>
2015-04-08 14:12:14 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'jrice@eol.org'
 license          'MIT'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'iptables'
