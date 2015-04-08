57946bfef9142794f12d69eaa9daacb19f450d06
JRice <jrice@eol.org>
2015-04-08 14:04:45 -0400

    add cookbook files

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,7 @@
-name             "eol-simple-iptables-wrapper"
-maintainer       "Marine Biological Laboratory"
-maintainer_email "jrice@eol.org"
-license          "MIT"
-description      "Manages iptables for EOL host machines"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
-depends          "iptables"
-
+name             'eol-iptables-wrapper'
+maintainer       'Marine Biological Laboratory'
+maintainer_email 'jrice@eol.org'
+license          'MIT'
+description      'Installs/Configures iptables'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
