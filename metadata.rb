b696d6d6bddb03ebc4d242f6c627af8c125a4cb6
JRice <jrice@eol.org>
2015-04-08 22:15:43 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'MIT'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends          'simple_iptables', '~> 0.7.1'
-version          '0.1.4'
+version          '0.1.5'
