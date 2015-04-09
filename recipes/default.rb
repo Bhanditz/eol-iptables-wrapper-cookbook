42149cfb107e4dd5cd93bfd4dce578028a08a69c
JRice <jrice@eol.org>
2015-04-08 22:05:20 -0400

    cannot use variable for rule name

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,77 +10,77 @@ end
 
 @node[:iptables][:allow_protocols].each do |protocol|
   case "http"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "http" do
       rule "--proto tcp --dport 80"
       jump "ACCEPT"
     end
   case "https"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "https" do
       rule "--proto tcp --dport 443"
       jump "ACCEPT"
     end
   case "http_8080"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "http_8080" do
       rule "--proto tcp --dport 8080"
       jump "ACCEPT"
     end
-  case "https_8081"
-    simple_iptables_rule protocol do
+  case "http_8081"
+    simple_iptables_rule "http_8081" do
       rule "--proto tcp --dport 8081"
       jump "ACCEPT"
     end
-  case "https_8082"
-    simple_iptables_rule protocol do
+  case "http_8082"
+    simple_iptables_rule "http_8082" do
       rule "--proto tcp --dport 8082"
       jump "ACCEPT"
     end
-  case "https_8083"
-    simple_iptables_rule protocol do
+  case "http_8083"
+    simple_iptables_rule "http_8083" do
       rule "--proto tcp --dport 8083"
       jump "ACCEPT"
     end
   case "rsync"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "rsync" do
       rule "--proto tcp --dport 22"
       jump "ACCEPT"
     end
   case "redis"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "redis" do
       rule "--proto tcp --dport 6379"
       jump "ACCEPT"
     end
   case "memcached"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "memcached" do
       rule "--proto tcp --dport 11211"
       jump "ACCEPT"
     end
   case "solr"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "solr" do
       rule "--proto tcp --dport 8983"
       jump "ACCEPT"
     end
   case "rabbit-mq"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "rabbit-mq" do
       rule "--proto tcp --dport 5671"
       jump "ACCEPT"
     end
   case "node.js"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "node.js" do
       rule "--proto tcp --dport 3000"
       jump "ACCEPT"
     end
   case "mysql"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "mysql" do
       rule "--proto tcp --dport 3306"
       jump "ACCEPT"
     end
   case "sysopia"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "sysopia" do
       rule "--proto tcp --dport 9999"
       jump "ACCEPT"
     end
   case "virtuoso"
-    simple_iptables_rule protocol do
+    simple_iptables_rule "virtuoso" do
       rule ["--proto tcp --dport 1111",
             "--proto tcp --dport 8890"]
       jump "ACCEPT"
