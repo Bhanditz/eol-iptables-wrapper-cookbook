9916e289d85608aaf08391b267c4b6d5acc4cf65
JRice <jrice@eol.org>
2015-04-08 22:02:36 -0400

    Complete rewrite.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'jrice@eol.org'
 license          'MIT'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
-depends          'iptables', '~> 1.0.1'
+depends          'simple_iptables', '~> 0.7.1'
+version          '0.1.4'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,129 @@
-include_recipe "iptables"
+include_recipe "simple_iptables"
 
-iptables_rule "defaults" do
-  action :enable
+# Always allow ssh (for now--we will whitelist this later, I suspect):
+simple_iptables_rule "ssh" do
+  rule "--proto tcp --dport 22"
+  jump "ACCEPT"
 end
+
+# Check for protocols:
+
+@node[:iptables][:allow_protocols].each do |protocol|
+  case "http"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 80"
+      jump "ACCEPT"
+    end
+  case "https"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 443"
+      jump "ACCEPT"
+    end
+  case "http_8080"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 8080"
+      jump "ACCEPT"
+    end
+  case "https_8081"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 8081"
+      jump "ACCEPT"
+    end
+  case "https_8082"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 8082"
+      jump "ACCEPT"
+    end
+  case "https_8083"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 8083"
+      jump "ACCEPT"
+    end
+  case "rsync"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 22"
+      jump "ACCEPT"
+    end
+  case "redis"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 6379"
+      jump "ACCEPT"
+    end
+  case "memcached"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 11211"
+      jump "ACCEPT"
+    end
+  case "solr"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 8983"
+      jump "ACCEPT"
+    end
+  case "rabbit-mq"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 5671"
+      jump "ACCEPT"
+    end
+  case "node.js"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 3000"
+      jump "ACCEPT"
+    end
+  case "mysql"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 3306"
+      jump "ACCEPT"
+    end
+  case "sysopia"
+    simple_iptables_rule protocol do
+      rule "--proto tcp --dport 9999"
+      jump "ACCEPT"
+    end
+  case "virtuoso"
+    simple_iptables_rule protocol do
+      rule ["--proto tcp --dport 1111",
+            "--proto tcp --dport 8890"]
+      jump "ACCEPT"
+    end
+  else
+    raise "I do not understand the #{protocol} protocol."
+  end
+end
+
+# Some intelligent defaults:
+
+simple_iptables_rule "established" do
+  rule "-m conntrack --ctstate ESTABLISHED,RELATED"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "ping" do
+  rule "--proto icmp"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "local" do
+  rule "--in-interface lo"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "conntrack ssh" do
+  rule "--proto tcp --dport 22 -m conntrack --ctstate NEW"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "prohibited" do
+  rule "--reject-with icmp-host-prohibited"
+  jump "REJECT"
+end
+
+simple_iptables_rule "forward prohibited" do
+  direction "FORWARD"
+  rule "--reject-with icmp-host-prohibited"
+  jump "REJECT"
+end
+
+simple_iptables_policy "INPUT" do
+  policy "REJECT"
+end
+
