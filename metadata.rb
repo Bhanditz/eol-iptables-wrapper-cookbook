053b75d17d8ab0e1c03a18efdda9597345dd3f77
JRice <jrice@eol.org>
2015-04-08 22:32:40 -0400

    Add attribute requirement to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,13 @@ license          'MIT'
 description      'Installs/Configures iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends          'simple_iptables', '~> 0.7.1'
+attribute        'iptables/allow_protocols',
+                   :display_name => "Allowed Protocols",
+                   :description => "List of the protocols allowed in IP Tables",
+                   :choice => %w(http https http_8080 http_8081 http_8082
+                                 http_8083 rsync redis memcached solr rabbit-mq
+                                 node.js mysql sysopia virtuoso),
+                   :type => "string",
+                   :required => "required"
+
 version          '0.1.5'
